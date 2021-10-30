aa9f6baf68eaa4c1f66f63e8f6da543287228594
Mesquita <mesquita@aospa.co>
2021-10-30 00:31:11 -0300

    aospa: Allow targets to disable GMS
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Change-Id: Ib47794986a63c265f7b8ce3c23193da750c45c01

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -56,7 +56,9 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 # Dexpreopt
 # Don't dexpreopt prebuilts. (For GMS).
+ifneq ($(TARGET_DISABLES_GMS), true)
 DONT_DEXPREOPT_PREBUILTS := true
+endif
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Launcher3QuickStep \
@@ -90,10 +92,12 @@ PRODUCT_PACKAGES += \
     vendor.aospa.power-service
 
 # Google - GMS, Pixel, and Mainline Modules
+ifneq ($(TARGET_DISABLES_GMS), true)
 $(call inherit-product, vendor/google/gms/config.mk)
 $(call inherit-product, vendor/google/pixel/config.mk)
 ifneq ($(TARGET_EXCLUDE_GMODULES), true)
 $(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules.mk)
+endif
 endif
 
 PRODUCT_PRODUCT_PROPERTIES += \

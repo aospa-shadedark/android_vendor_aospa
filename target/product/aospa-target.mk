b57181aa08e1f03840a7af4ced55eb22745b7f4d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-10-10 15:12:23 +0530

    target: Build NTFS binaries
    Requires external/ntfs-3g
    Sepolicy taken from https://github.com/LineageOS/android_device_lineage_sepolicy
    
    Change-Id: Ib27f9c75046f5bcf1d77274c9bcfec2c77e1a049

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -119,6 +119,12 @@ SYSTEMUI_OPTIMIZE_JAVA := true
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.arm64.memtag.system_server=off
 
+# NTFS
+PRODUCT_PACKAGES += \
+    fsck.ntfs \
+    mkfs.ntfs \
+    mount.ntfs
+
 # One Handed Mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true

5543eb149ce7d0eb9dd1a1d78004cb4c012028b1
songlongdaiduong <song.long.dai.duong@gmail.com>
2025-03-21 14:47:34 +0800

    fonts: Add Comfortaa font

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -13,6 +13,7 @@ PRODUCT_PACKAGES += \
     AOSPASettingsOverlay \
     AOSPASettingsProviderOverlay \
     AOSPASystemUIOverlay \
+    FontComfortaaOverlay \
     FontExotwoOverlay \
     FontGoogleSansOverlay \
     FontHarmonySansOverlay \

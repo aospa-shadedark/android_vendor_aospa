c34ce2405b16ab2ce55d69b8ad0b4f78497643f3
ShadeDark6 <nizamfa6@gmail.com>
2026-05-16 05:35:37 +0000

    aospa: target: Add BCR
    Change-Id: Iefe107a8b46e8d47eea42bd8be46e1159915215d

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -31,6 +31,9 @@ endif
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.config.media_vol_steps=30
 
+# BCR
+$(call inherit-product, vendor/bcr/bcr.mk)
+
 # Boot Animation
 $(call inherit-product, vendor/aospa/bootanimation/bootanimation.mk)
 
